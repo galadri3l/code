fc0304e94068bb232290670a2e75525262a75e60
galadri3l <yashtripathi1608@gmail.com>
2016-08-25 14:55:59 +0530

    trying to fix personal bongo fixes

--- linux/.emacs.d/lisp/create-tags.el
+++
@@ -1,7 +0,0 @@
-(defun create-tags (dir-name)
-    "Create tags file."
-    (interactive "DDirectory: ")
-	(let ((path-to-ctags (concat dir-name "TAGS")))
-    (shell-command
-     (format "ctags -f %s -e -R %s --exclude=db --exclude=test --exclude=.git --exclude=public" path-to-ctags (directory-file-name dir-name)))))
-(provide 'create-tags)
